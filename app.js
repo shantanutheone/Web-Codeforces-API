be5c56e772f4c1be64900078fd19d4ba8acb978b
shantanu <shaantanutripathi@gmail.com>
2020-07-14 00:37:40 +0530

    Added Authentication Feature

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 var request = require("request")
 
-request("https://codeforces.com/api/blogEntry.comments?blogEntryId=79", function(error, response, body){
+// Added a npm package to handle Authentication
+// =======================================================================
+var CFAuth = require('./node_modules/cf-auth/index.js');
+var keys = require('./node_modules/cf-auth/keys.js');
+
+var auth = CFAuth(keys);
+
+
+// =========================================================================
+
+// These two line should be added if you want to add authentication with change in target
+var target = 'https://codeforces.com/api/user.friends?handle=404PageNotFound';
+var url = auth.genURL(target);
+
+console.log(url);
+
+request(url, function(error, response, body){
     if(!error && response.statusCode == 200){
         var data = JSON.parse(body);
         console.log(data);
